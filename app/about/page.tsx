1a0c4d14bb4e8fde62b58cfc2b0b9b1d53437f31
Dylan <79028721+dylanrstevens@users.noreply.github.com>
2025-01-20 12:29:13 -0800

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,15 +16,15 @@ export default function AboutPage() {
 						<br/><br/>
 					</h1>
 					<h1 className="text-justify text-lg font-semibold text-default-500">
-						I have been working in the technology field for 2 years. I currently have a bachelor of science (B.Sc.) in computer science from the University of Victoria, British Columbia. I&apos;ve previously worked as a front end web application developer for an 8-month co-op contract with the BC Pension Corporation, and as a front end voice-app python developer for L1 Scientific LTD (see more in <Link underline="hover" href="/resume" size="lg" color={"primary"}>resumé</Link> section).
+						I currently have a bachelor of science (B.Sc.) in computer science from the University of Victoria, British Columbia. I am currently working as a Solutions Engineer for MiCROTEC US in Oregon.
+						<br></br>
+						I&apos;ve previously worked as a front end web application developer for the BC Pension Corporation, and as a voice-app python developer for L1 Scientific LTD (see more in <Link underline="hover" href="/resume" size="lg" color={"primary"}>resumé</Link> section).
 						<br/><br/>
-						Living in British Columbia, I&apos;m currently based in the pacific northwest area, and hope to contribute to the growing technology on the west coast of the US and Canada. 
-						<br/><br/>
-						I&apos;m particularly interested in software development that focuses on designing and programming software for embedded systems, and other specialized computing devices within larger systems or products. I have work experience developing for specialized computing devices, cloud computing, and web development. 
+						My interests focus on designing and programming software solutions for embedded systems, and other specialized computing devices within larger systems or products, and the impact that eco-technology has on environmental industries. I have work experience developing for specialized computing devices, cloud computing, and web development. 
 						<br/><br/>
 						Outside of the work force, I&apos;m passionate about skiing, biking, and many other outdoor recreations.
 						<br/><br/>
-						Thank you for visiting my website and reading about me. Please feel free to send me an email (dylan@dylanstevens.ca) to reach out, and check out my resume for a record of my skills and employment history.
+						Thank you for visiting my website and reading about me. Please feel free to send me an email (dylan@dylanstevens.ca) to reach out.
 					</h1>
 				</div>
 			</div>
